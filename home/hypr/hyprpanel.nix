009d8aa616d51708b757a5aca0880e7f610888b0
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2025-10-19 18:11:19 +0200

    Working hyprpanel conf

--- home/hypr/hyprpanel.nix
+++ home/hypr/hyprpanel.nix
@@ -4,45 +4,60 @@
 	enable = true;
 	settings = {
       bar.layouts = {
-        "DP-1" = {
+        "0" = {
           left = [ "dashboard" "workspaces" ];
           middle = [ "media" ];
-          right = [ "volume" "network" "bluetooth" "clock" "notifications" ];
+          right = [ "volume" "network" "bluetooth" "systray" "clock" "notifications" ];
         };
-		"DP-3" = {
-          left = [ "dashboard" "workspaces" ];
-          middle = [ "media" ];
-          right = [ "clock" "notifications" ];
-        };
-		"HDMI-A-1" = {
+		"1" = {
           left = [ "dashboard" "workspaces" ];
           middle = [ "media" ];
           right = [ "clock" "notifications" ];
         };
-      };
-
-      bar.launcher.autoDetectIcon = true;
-      bar.workspaces.show_icons = true;
-
-      menus.clock = {
-        time = {
-          military = true;
-          hideSeconds = true;
+		"2" = {
+          left = [ "dashboard" "workspaces" ];
+          middle = [ "media" ];
+          right = [ "clock" "notifications" ];
         };
-        weather.unit = "metric";
       };
-	
-	  menus.systray.ignoreList = [ "battery" ];
-
-      menus.dashboard.directories.enabled = false;
-      menus.dashboard.stats.enable_gpu = true;
-
-      theme.bar.transparent = true;
-
-      theme.font = {
-        name = "CaskaydiaCove NF";
-        size = "16px";
-      };
+	  theme.bar.buttons.enableBorders = true;
+	  bar.workspaces.applicationIconMap = {};
+	  bar.workspaces.workspaceIconMap = {};
+	  bar.workspaces.workspaces = 5;
+	  bar.workspaces.scroll_speed = 5;
+	  bar.volume.label = true;
+	  bar.network.showWifiInfo = false;
+	  bar.clock.format = "%a %b %d  %H:%M";
+	  bar.media.show_label = true;
+	  bar.media.format = "{artist: - }{title}";
+	  bar.notifications.show_total = true;
+	  bar.media.show_active_only = true;
+	  menus.transition = "crossfade";
+	  theme.bar.menus.enableShadow = false;
+	  menus.media.hideAuthor = false;
+	  menus.media.displayTimeTooltip = false;
+	  menus.media.displayTime = false;
+	  notifications.ignore = [
+		"spotify"
+	  ];
+	  notifications.active_monitor = false;
+	  notifications.showActionsOnHover = false;
+	  theme.osd.duration = 2500;
+	  theme.osd.enable = false;
+	  menus.volume.raiseMaximumVolume = true;
+	  menus.clock.time.military = true;
+	  menus.clock.weather.unit = "metric";
+	  menus.clock.weather.location = "47.436424,9.130866";
+	  menus.clock.weather.key = "/etc/nixos/weather.json";
+	  menus.dashboard.controls.enabled = true;
+	  menus.dashboard.shortcuts.left.shortcut1.tooltip = "Firefox";
+	  menus.dashboard.shortcuts.left.shortcut1.command = "firefox";
+	  menus.dashboard.shortcuts.left.shortcut1.icon = "";
+	  menus.dashboard.directories.enabled = false;
+	  menus.dashboard.shortcuts.enabled = false;
+	  menus.dashboard.stats.enabled = false;
+	  bar.customModules.cpuTemp.sensor = "/dev/cpu_temp";
+	  menus.clock.weather.interval = 120000;
 	};
   };
 }
