421db4fd1689efbd1f52ffc9f420cfbc2771e3c8
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2025-10-31 19:22:14 +0100

    Network settings

--- configuration.nix
+++ configuration.nix
@@ -10,21 +10,12 @@
       ./hardware-configuration.nix
 	  ./shares.nix
 	  ./virtualisation.nix
+	  ./network.nix
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Zurich";
@@ -237,6 +228,7 @@ in {
 	virt-manager
 	virt-viewer
 	spice-gtk
+	dig
   ];
 
   virtualisation.docker.enable = true;
---
+++ network.nix
@@ -0,0 +1,17 @@
+{
+  networking = {
+    hostName = "nixos";
+	networkmanager.enable = true;
+	networkmanager.dns = "none";
+
+	nameservers = [
+	  "192.168.1.148"
+	  "fd32:9975:719f:0:7a55:36ff:fe02:15f3"
+	  "1.1.1.1"
+	  "2606:4700:4700::1111"
+	  "1.0.0.1"
+	  "2606:4700:4700::1001"
+	];
+  };
+  services.dnsmasq.resolveLocalQueries = false;
+}
