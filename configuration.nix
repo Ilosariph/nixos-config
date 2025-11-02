57541614e595a1298ab2e7f9b0459ba13ff4fae4
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2025-11-02 23:36:30 +0100

    Configure additional drive

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+	  ./drives.nix
 	  ./shares.nix
 	  ./virtualisation.nix
 	  ./network.nix
---
+++ drives.nix
@@ -0,0 +1,10 @@
+{
+  fileSystems."/data" = {
+   device = "/dev/disk/by-uuid/533a892a-9b2c-40c5-90a1-cb6f2df36f32";
+   fsType = "ext4";
+   options = [
+     "users"
+     "nofail"
+   ];
+ };
+}
