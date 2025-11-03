de370bd85ce3df0c35631be0a33fe50980682803
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2025-11-03 21:36:08 +0100

    Fix wivrn by removing bad default config

--- configuration.nix
+++ configuration.nix
@@ -146,28 +146,6 @@ in {
 
     # Run WiVRn as a systemd service on startup
     autoStart = true;
-
-    # Config for WiVRn (https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md)
-    config = {
-      enable = true;
-      json = {
-        # 1.0x foveation scaling
-        scale = 1.0;
-        # 100 Mb/s
-        bitrate = 100000000;
-        encoders = [
-          {
-            encoder = "vaapi";
-            codec = "h265";
-            # 1.0 x 1.0 scaling
-            width = 1.0;
-            height = 1.0;
-            offset_x = 0.0;
-            offset_y = 0.0;
-          }
-        ];
-      };
-    };
   };
 
   xdg.portal = {
