d7b06878d70caabd7e51053bce7826b801d20497
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2025-11-22 02:57:54 +0100

    Improve yazi preview

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   programs.yazi.enable = true;
   programs.yazi.keymap = {
@@ -12,5 +13,14 @@
 	  }
 	  # { run = [ "'shell -- for path in \"$@\"; do echo \"file://$path\"; done | wl-copy -t text/uri-list'" "yank" ]; on = [ "y" ]; }
 	];
+  };
+  programs.yazi.settings = {
+	preview = {
+	  cache_dir = "${config.xdg.cacheHome}/yazi/preview-cache";
+	  max_width = 1920;
+	};
+	tasks = {
+	  image_bound = [0 0];
+	};
   };
 }
