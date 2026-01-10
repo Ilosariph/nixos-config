e58a851e4d26db3dadd083457045b061ccdd1136
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2026-01-10 17:41:14 +0100

    file share for scans

--- with-desktop/config/shares.nix
+++ with-desktop/config/shares.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/mnt/projects" = { # Your desired mount point
+  fileSystems."/mnt/projects" = {
     device = "//192.168.1.148/p";
     fsType = "cifs";
     options = let
@@ -13,13 +13,25 @@
       "vers=3.0"
     ];
   };
-  fileSystems."/mnt/simon" = { # Your desired mount point
+  fileSystems."/mnt/simon" = {
     device = "//192.168.1.148/simon_data";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
     in [
       "${automount_opts},credentials=/etc/nixos/smb-s"
+      "uid=1000"
+      "gid=100"
+      "vers=3.0"
+    ];
+  };
+  fileSystems."/mnt/scan" = {
+    device = "//192.168.1.95/ScansLaserjet";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in [
+      "${automount_opts},credentials=/etc/nixos/smb-scan"
       "uid=1000"
       "gid=100"
       "vers=3.0"
