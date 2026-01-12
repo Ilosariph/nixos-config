120ec6f195b22eb957f9702a6d34eddb5c034851
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2026-01-12 21:28:56 +0100

    write ssh public key to correct file

--- no-desktop/machines/laptopserver/configuration.nix
+++ no-desktop/machines/laptopserver/configuration.nix
@@ -6,14 +6,16 @@
 	boot.kernelPackages = pkgs.linuxPackages_latest;
 
 	sops = {
-    age.keyFile = "/home/simon/.config/sops/age/keys.txt";
+		age.keyFile = "/home/simon/.config/sops/age/keys.txt";
+		defaultSopsFile = ../../../secrets.yaml;
 
-    defaultSopsFile = ../../../secrets.yaml;
-
-    secrets.nucserver-ssh-public-key = {
-			path = "/home/simon/.ssh/id_ed25519.pub";
-    };
-  };
+		secrets.nucserver-ssh-public-key = {
+			path = "/home/simon/.ssh/authorized_keys";
+			owner = "simon";
+			group = "users";
+			mode = "0400";
+		};
+	};
 
 	services.openssh.enable = true;
 }
