95c316cfd0032a99cf3cfd10bdb312b5eec410fd
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2026-02-16 18:34:51 +0100

    disable autoconnect for vpn configs

--- with-desktop/machines/general/wireguard/vpn.nix
+++ with-desktop/machines/general/wireguard/vpn.nix
@@ -1,29 +1,34 @@
 { pkgs, ... }:
 {
-  system.activationScripts.importWireguardVPNS = {
-    deps = [ "etc" ];
-    text = ''
-      NMCLI=${pkgs.networkmanager}/bin/nmcli
+	system.activationScripts.importWireguardVPNS = {
+		deps = [ "etc" ];
+		text = ''
+			NMCLI=${pkgs.networkmanager}/bin/nmcli
 
-      import_vpn() {
-        NAME="$1"
-        FILE="/etc/wireguard/$1.conf"
+			import_vpn() {
+				NAME="$1"
+				FILE="/etc/wireguard/$1.conf"
 
-        if [ -f "$FILE" ]; then
-          echo "Importing $NAME from $FILE"
+				if [ -f "$FILE" ]; then
+					echo "Importing $NAME from $FILE"
 
-          # Delete existing connection if present
-          $NMCLI connection delete "$NAME" >/dev/null 2>&1 || true
+					# Delete existing connection if present
+					$NMCLI connection delete "$NAME" >/dev/null 2>&1 || true
 
-          # Import fresh
-          $NMCLI connection import type wireguard file "$FILE"
-        else
-          echo "Skipping $NAME (no config found at $FILE)"
-        fi
-      }
+					# Import fresh
+					$NMCLI connection import type wireguard file "$FILE"
 
-      import_vpn home
-      import_vpn proton
-    '';
-  };
+					# Disable autoconnect
+					$NMCLI connection modify "$NAME" connection.autoconnect no
+
+					echo "Autoconnect disabled for $NAME"
+				else
+					echo "Skipping $NAME (no config found at $FILE)"
+				fi
+			}
+
+			import_vpn home
+			import_vpn proton
+		'';
+	};
 }
