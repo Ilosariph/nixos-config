faabc0e9110172d6336418b87e39a125692c5903
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2026-02-17 22:01:13 +0100

    Max boot resolution on x86

--- general/bootloader/systemd.nix
+++ general/bootloader/systemd.nix
@@ -2,7 +2,7 @@
 {
   config = lib.mkIf (config.dotfiles.bootloader == "systemd") {
     boot.loader.systemd-boot.enable = true;
-    # boot.loader.systemd-boot.consoleMode = "max";
+    boot.loader.systemd-boot.consoleMode = if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then "max" else "0";
     boot.loader.efi.canTouchEfiVariables = if pc == "macbook" then false else true;
   };
 }
