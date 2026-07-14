d05bf07b3cc9ca0a3ef7a61a49d14c7692878dad
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2026-07-14 22:10:24 +0200

    initial hermes setup

--- modules/machines/evo/configuration.nix
+++ modules/machines/evo/configuration.nix
@@ -6,11 +6,6 @@
 
   users.users.${config.dotfiles.user.name}.extraGroups = [ "docker" ];
 
-  environment.sessionVariables = {
-    OLLAMA_HOST = "http://127.0.0.1:11435";
-    OLLAMA_MODELS = "/var/lib/ollama/models";
-  };
-
   environment.systemPackages = with pkgs; [
     nodejs_22
     rocmPackages.rocminfo
@@ -32,7 +27,4 @@
 	hardware.graphics.extraPackages = with pkgs; [
 	  rocmPackages.clr.icd
 	];
-
-	# Ollama port for nanobot
-	networking.firewall.interfaces.docker0.allowedTCPPorts = [ 3001 ];
 }
